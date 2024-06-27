60890b26cddc1661a78ecb565618b25b38816d5d
Nanakzy <nanakzy@gmail.com>
2024-06-27 13:35:36 +0000

    t
    tsk

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;  // Declare using `let` for block scope
+  let task2 = true;  // Declare using `let` for block scope
 
   if (trueOrFalse) {
-    const innerTask = true;
-    const innerTask2 = false;
+    let task = true;  // Declare using `let` to create a new variable within the block
+    let task2 = false; // Declare using `let` to create a new variable within the block
   }
 
   return [task, task2];
