94d16cf530b589237dd920708ca0d0832330151f
Nanakzy <nanakzy@gmail.com>
2024-06-27 19:44:28 +0000

    it
    correction

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,25 +1,25 @@
 export default class Car {
   constructor(brand, motor, color) {
-    this["_brand"] = brand;
-    this["_motor"] = motor;
-    this["_color"] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   get brand() {
-    return this["_brand"];
+    return this._brand;
   }
 
   get motor() {
-    return this["_motor"];
+    return this._motor;
   }
 
   get color() {
-    return this["_color"];
+    return this._color;
   }
 
   cloneCar() {
     const Constructor = this.constructor;
-    const clonedCar = new Constructor(this["_brand"], this["_motor"], this["_color"]);
+    const clonedCar = new Constructor(this._brand, this._motor, this._color);
     return clonedCar;
   }
 }
