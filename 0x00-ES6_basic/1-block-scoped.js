d1c130fc21f775a45f269bc6bc5c0bf25fb71657
Nanakzy <nanakzy@gmail.com>
2024-06-27 13:28:50 +0000

    t
    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    const innerTask = true;
+    const innerTask2 = false;
   }
 
   return [task, task2];
