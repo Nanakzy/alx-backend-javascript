4c2b09668705cb8b0acc9c92d2b38af86c569f05
Nanakzy <nanakzy@gmail.com>
2024-08-20 19:33:16 +0000

    redone

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,5 @@
-/**
- * Displays a message on the STDOUT.
- * @param {String} message is  The message to display.
- * @author Nana <https://github.com/Nanakzy>
- */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
