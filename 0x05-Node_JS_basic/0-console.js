bfb8328d6bb91eebbb3788b168e33b58c6890e52
Nanakzy <nanakzy@gmail.com>
2024-08-20 19:28:22 +0000

    dated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @param {String} message is  The message to display.
+ * @author Nana <https://github.com/Nanakzy>
  */
 const displayMessage(message) {
     console.log(message);
