dcebe197abeb3d0e713854634164d9112c348225
Nanakzy <nanakzy@gmail.com>
2024-08-22 17:54:40 +0000

    task9

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (!isNaN(id)) {
+    res.send(`Payment methods for cart ${id}`);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
