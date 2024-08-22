c6cc457b5d5787705fd535e125ad433dc5125a21
Nanakzy <nanakzy@gmail.com>
2024-08-22 17:57:28 +0000

    task10

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
@@ -13,6 +15,20 @@ app.get('/cart/:id', (req, res) => {
   } else {
     res.status(404).send('Not Found');
   }
+});
+
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
 });
 
 app.listen(port, () => {
